5ba6a88a94b932185083b709202d8fa56765cce9
92radar <viepro.db@gmail.com>
2026-08-19 15:09:34 +0200

    Add DocumentFormType and extend RentalFileFormType with new form fields

---
+++ src/Form/RentalFileFormExtension.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace aintreallydown\DocumentBundle\Form;
+
+use App\Form\RentalFileFormType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class RentalFileFormTypeExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [RentalFileFormType::class];
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+
+        $builder
+            ->add('documents', TextType::class, [
+                'mapped' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => RentalFile::class,
+            'method' => 'PATCH',
+        ]);
+    }
+}
